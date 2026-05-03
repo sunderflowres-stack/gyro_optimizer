f6437008bb46f4ae15c38b7f67200f0f2c018658
Sunder_Flow_Research <sunder.flow.res@gmail.com>
2026-05-03 13:42:56 +0500

    Update __init__.py

--- gyro/__init__.py
+++ gyro/__init__.py
@@ -1,8 +1,4 @@
 from .gyro_adam import GYROAdam
 from .gyro_sgd import GYROSGD
 
-# Legacy aliases for backward compatibility
-CliffordAdam = GYROAdam
-CliffordSGD = GYROSGD
-
-__all__ = ['GYROAdam', 'GYROSGD', 'CliffordAdam', 'CliffordSGD']
+__all__ = ['GYROAdam', 'GYROSGD']
